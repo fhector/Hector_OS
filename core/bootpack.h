4a7a6052f4034153ddc2b70767d586aec2d49237
hector <f_hector@hotmail.com>
2015-03-02 21:42:24 +0800

    11day_01
    refine mouse boundary display & refine assign ctl(sheet)

--- core/bootpack.h
+++ core/bootpack.h
@@ -140,12 +140,12 @@ int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
 unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size);
 int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
 
-/*sheet.c*/
-#define MAX_SHEETS 256
-struct SHEET{
-	unsigned char * buf;
-	int bxsize,bysize,vx0,vy0,col_inv,height,flags;
-	};
+/* sheet.c */
+#define MAX_SHEETS		256
+struct SHEET {
+	unsigned char *buf;
+	int bxsize, bysize, vx0, vy0, col_inv, height, flags;
+	struct SHTCTL *ctl;};
 struct SHTCTL{
 	unsigned char *vram;
 	int xsize,ysize,top;
@@ -155,7 +155,7 @@ struct SHTCTL{
 struct SHTCTL *shtctl_init(struct MEMMAN *memman,unsigned char *vram,int xsize,int ysize);
 struct SHEET *sheet_alloc(struct SHTCTL *ctl);
 void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, int col_inv);
-void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height);
-void sheet_refresh(struct SHTCTL *ctl, struct SHEET *sht, int bx0, int by0, int bx1, int by1);
-void sheet_slide(struct SHTCTL *ctl, struct SHEET *sht, int vx0, int vy0);
-void sheet_free(struct SHTCTL *ctl, struct SHEET *sht);
+void sheet_updown(struct SHEET *sht, int height);
+void sheet_refresh(struct SHEET *sht, int bx0, int by0, int bx1, int by1);
+void sheet_slide(struct SHEET *sht, int vx0, int vy0);
+void sheet_free(struct SHEET *sht);
