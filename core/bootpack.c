883346be75c52cbdb37739c05e06f6ee67cb85a2
hector <f_hector@hotmail.com>
2015-03-02 22:57:20 +0800

    12day_01
    add timer( measure time & set timeout)

--- core/bootpack.c
+++ core/bootpack.c
@@ -8,9 +8,10 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title);
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
-	char s[40], keybuf[32], mousebuf[128];
+	struct FIFO8 timerfifo;
+	char s[40], keybuf[32], mousebuf[128], timerbuf[8];
 	int mx, my, i;
-	unsigned int memtotal, count = 0;
+	unsigned int memtotal;
 	struct MOUSE_DEC mdec;
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 	struct SHTCTL *shtctl;
@@ -22,8 +23,11 @@ void HariMain(void)
 	io_sti(); /* IDT/PIC initial finish, and so CPU sti*/
 	fifo8_init(&keyfifo, 32, keybuf);
 	fifo8_init(&mousefifo, 128, mousebuf);
-	io_out8(PIC0_IMR, 0xf9); /* set PIC1& keyboard(11111001) */
+	init_pit();
+	io_out8(PIC0_IMR, 0xf8); /* set PIC1& keyboard(11111000) */
 	io_out8(PIC1_IMR, 0xef); /* set mouse(11101111) */
+	fifo8_init(&timerfifo, 8, timerbuf);
+	settimer(1000, &timerfifo, 1);
 
 	init_keyboard();
 	enable_mouse(&mdec);
@@ -61,14 +65,13 @@ void HariMain(void)
 	sheet_refresh(sht_back, 0, 0, binfo->scrnx, 48);
 
 	for (;;) {
-		count++;
-		sprintf(s, "%010d", count);
+		sprintf(s, "%010d", timerctl.count);
 		boxfill8(buf_win, 160, COL8_C6C6C6, 40, 28, 119, 43);
 		putfonts8_asc(buf_win, 160, 40, 28, COL8_000000, s);
 		sheet_refresh(sht_win, 40, 28, 120, 44);
 
 		io_cli();
-		if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) == 0) {
+		if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) + fifo8_status(&timerfifo) == 0) {
 			io_sti();
 		} else {
 			if (fifo8_status(&keyfifo) != 0) {
@@ -117,6 +120,11 @@ void HariMain(void)
 					sheet_refresh(sht_back, 0, 0, 80, 16);
 					sheet_slide(sht_mouse, mx, my);
 				}
+			} else if (fifo8_status(&timerfifo) != 0) {
+				i = fifo8_get(&timerfifo); /* first read(In order to set the starting point) */
+				io_sti();
+				putfonts8_asc(buf_back, binfo->scrnx, 0, 64, COL8_FFFFFF, "10[sec]");
+				sheet_refresh(sht_back, 0, 64, 56, 80);
 			}
 		}
 	}
