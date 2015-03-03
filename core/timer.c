e67325dedc8fae25c6299f5941624c729d4053ee
hector <f_hector@hotmail.com>
2015-03-03 20:39:27 +0800

    13day_03
    Improve the interrupt processing performance

--- core/timer.c
+++ core/timer.c
@@ -13,15 +13,20 @@ struct TIMERCTL timerctl;
 void init_pit(void)
 {
 	int i;
+	struct TIMER *t;
 	io_out8(PIT_CTRL, 0x34);
 	io_out8(PIT_CNT0, 0x9c);
 	io_out8(PIT_CNT0, 0x2e);
 	timerctl.count = 0;
-	timerctl.next = 0xffffffff; /* The original timer is not running */
-	timerctl.using = 0;
 	for (i = 0; i < MAX_TIMER; i++) {
 		timerctl.timers0[i].flags = 0; /* unused */
 	}
+	t = timer_alloc(); 
+	t->timeout = 0xffffffff;
+	t->flags = TIMER_FLAGS_USING;
+	t->next = 0; /* the end */
+	timerctl.t0 = t; /* Because now only the sentinel, so it is in the front */
+	timerctl.next = 0xffffffff; /* Because now only the sentinel, so the next is just sentinel */
 	return;
 }
 
@@ -52,55 +57,54 @@ void timer_init(struct TIMER *timer, struct FIFO32 *fifo, int data)
 
 void timer_settime(struct TIMER *timer, unsigned int timeout)
 {
-	int e, i, j;
+	int e;
+	struct TIMER *t, *s;
 	timer->timeout = timeout + timerctl.count;
 	timer->flags = TIMER_FLAGS_USING;
 	e = io_load_eflags();
 	io_cli();
-	/* Search the registered location */
-	for (i = 0; i < timerctl.using; i++) {
-		if (timerctl.timers[i]->timeout >= timer->timeout) {
-			break;
+	t = timerctl.t0;
+	if (timer->timeout <= t->timeout) {
+		/* Insert the front */
+		timerctl.t0 = timer;
+		timer->next = t; /* next is set t */
+		timerctl.next = timer->timeout;
+		io_store_eflags(e);
+		return;
+	}
+	/* Search the insert position */
+	for (;;) {
+		s = t;
+		t = t->next;
+		if (timer->timeout <= t->timeout) {
+			/* insert between s and t */
+			s->next = timer; 
+			timer->next = t; 
+			io_store_eflags(e);
+			return;
 		}
 	}
-	/* After I back a bit */
-	for (j = timerctl.using; j > i; j--) {
-		timerctl.timers[j] = timerctl.timers[j - 1];
-	}
-	timerctl.using++;
-	/* Inserted into the space */
-	timerctl.timers[i] = timer;
-	timerctl.next = timerctl.timers[0]->timeout;
-	io_store_eflags(e);
-	return;
 }
 
 void inthandler20(int *esp)
 {
-	int i, j;
+	struct TIMER *timer;
 	io_out8(PIC0_OCW2, 0x60);	/* IRQ-00 inform GIC */
 	timerctl.count++;
 	if (timerctl.next > timerctl.count) {
 		return;
 	}
-	for (i = 0; i < timerctl.using; i++) {
-		/* all timers is using, no need confirm flags */
-		if (timerctl.timers[i]->timeout > timerctl.count) {
+	timer = timerctl.t0; /* First turn on the front of addresses assigned to the timer */
+	for (;;) {	
+		if (timer->timeout > timerctl.count) {
 			break;
 		}
 		/* timeout */
-		timerctl.timers[i]->flags = TIMER_FLAGS_ALLOC;
-		fifo32_put(timerctl.timers[i]->fifo, timerctl.timers[i]->data);
+		timer->flags = TIMER_FLAGS_ALLOC;
+		fifo32_put(timer->fifo, timer->data);
+		timer = timer->next; 
 	}
-	/* Just have n timer timeout, others move */
-	timerctl.using -= i;
-	for (j = 0; j < timerctl.using; j++) {
-		timerctl.timers[j] = timerctl.timers[i + j];
-	}
-	if (timerctl.using > 0) {
-		timerctl.next = timerctl.timers[0]->timeout;
-	} else {
-		timerctl.next = 0xffffffff;
-	}
+	timerctl.t0 = timer;
+	timerctl.next = timer->timeout;
 	return;
 }
